83fc49c1d7e9eeacc03378a607405ea1986b5158
NoopuraRose <noopura.rose@gmail.com>
2026-06-05 18:31:06 -0700

    project completed

--- api_bp_predictor_linreg.py
+++ api_bp_predictor_linreg.py
@@ -1,6 +1,22 @@
 import joblib
+from flask import Flask,request
 
+app = Flask(__name__)
 model = joblib.load("age_bp_predictor_lin_reg.pkl")
 
-new_bp = model.predict([[160]])
-print("Predicted bp = ", new_bp[0][0])
+@app.route("/test", methods = ["GET"])
+def test():
+    return "hello"
+
+@app.route("/user", methods = ["POST"])
+def user():
+    input = request.get_json()
+    print(input)
+    age = input.get("age")
+    print(age)
+    new_bp = model.predict([[age]])
+    print("Predicted bp = ", new_bp[0][0])
+    return {"bp": new_bp[0][0]}
+
+if __name__ == "__main__":
+    app.run()
